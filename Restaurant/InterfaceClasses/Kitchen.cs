ad897be35e3457ecc0b8b3fd7cfbfa2f792ef05b
vaki <andrey_gr2019@mail.ru>
2025-10-04 21:29:38 +0700

    Меню заказов вынесено в отдельный класс Добавление заказа изменено

--- Restaurant/InterfaceClasses/Kitchen.cs
+++ Restaurant/InterfaceClasses/Kitchen.cs
@@ -9,13 +9,13 @@ namespace Restaurant.InterfaceClasses
 {
     internal class Kitchen : IKitchen
     {
-        private FoodMenu menu;
-        private List<Food> orders;
+        private FoodMenu foodMenu;
+        private OrdersMenu ordersMenu;
 
         public Kitchen()
         {
-            menu = new FoodMenu();
-            orders = new List<Food>();
+            foodMenu = new FoodMenu();
+            ordersMenu = new OrdersMenu();
         }
 
         public static void WaitForUser()
@@ -68,10 +68,10 @@ namespace Restaurant.InterfaceClasses
                 switch (choice)
                 {
                     case "1":
-                        menu.ShowFoodMenu(this);
+                        foodMenu.ShowFoodMenu(this);
                         break;
                     case "2":
-                        ShowOrdersMenu();
+                        ordersMenu.ShowOrdersMenu();
                         break;
                     case "3":
                         return;
@@ -87,112 +87,9 @@ namespace Restaurant.InterfaceClasses
         // добавляет блюдо в список заказов из списка меню по индексу
         internal void MakeOrderByItem(Food foodItem)
         {
-            switch (foodItem)
-            {
-                case Pizza pizza:
-                    var (pizzaName, size) = pizza;
-                    orders.Add(new Pizza(pizzaName, size));
-                    break;
-                case Calzone calzone:
-                    var (calzoneName, calzoneWeight) = calzone;
-                    orders.Add(new Calzone(calzoneName, calzoneWeight));
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        // вывод меню взаимодействия с заказами
-        private void ShowOrdersMenu()
-        {
-            int i = 0, menuChoice;
-
-            Console.Clear();
-            Console.WriteLine("Меню взаимодействия с заказами:");
-            for (; i < orders.Count; i++)
-            {
-                Console.Write(i + 1 + ".");
-                orders[i].PrintFoodShort(); // выводим элементы списка заказов
-            }
-            Console.WriteLine(++i + ".Выйти в основное меню.");
-
-            Console.Write("Ваш выбор (цифра 1-{0}): ", i);
-            string? choiceStr = Console.ReadLine(); // получаем строку с выбором
-
-            menuChoice = CheckChoiseMenu(choiceStr, i);
-
-            // if (menuChoice <= menu.Count) MakeOrderByIndex(menuChoice - 1); // если выбор не вышел за пределы списка меню
-            if (menuChoice <= orders.Count) ShowOrderMenuByIndex(menuChoice - 1); // если выбор не вышел за пределы списка меню
+            ordersMenu.MakeOrderByItem(foodItem);
         }
 
         
-
-        // Вывод информации и действия с заказом
-        private void ShowOrderMenuByIndex(int index)
-        {
-                int menuChoice, i = 3;
-
-                Console.Clear();
-                orders[index].PrintFoodShort();
-                Console.WriteLine("Меню заказа:");
-                Console.WriteLine("1.Вывести подробную информацию");
-                Console.WriteLine("2.Взаимодействовать с заказом");
-                Console.WriteLine("3.Выйти в главное меню");
-
-                Console.Write("Ваш выбор (цифра 1-{0}): ", i);
-                string? choiceStr = Console.ReadLine(); // получаем строку с выбором
-
-                menuChoice = CheckChoiseMenu(choiceStr, i);
-
-                if (menuChoice == 1) ShowFullOrderInfo(index);
-                else if (menuChoice == 2) ShowInteractionMenuByIndex(index);
-                else if (menuChoice == 3) return;
-        }
-
-        // вывести информацию о заказе
-        private void ShowFullOrderInfo(int index)
-        {
-            Console.Clear();
-            Console.WriteLine("Полная информация о заказе:");
-            orders[index].PrintFoodFull();
-            Console.WriteLine("Далее...");
-            Console.ReadKey();
-        }
-
-
-
-        // вывести меню взаимодействия с заказами
-        private void ShowInteractionMenuByIndex(int index)
-        {
-            int menuChoice, i = 4;
-            Food order = orders[index];
-
-            Console.Clear();
-            order.PrintFoodShort();
-            Console.WriteLine("Варианты взаимодействия:");
-            Console.WriteLine("1.Запечь");
-            Console.WriteLine("2.Нарезать");
-            Console.WriteLine("3.Съесть");
-            Console.WriteLine("4.Выйти из меню");
-
-            Console.Write("Ваш выбор (цифра 1-{0}): ", i);
-            string? choiceStr = Console.ReadLine(); // получаем строку с выбором
-
-            menuChoice = CheckChoiseMenu(choiceStr, i);
-
-            if (menuChoice == 1) order.Bake();
-            else if (menuChoice == 2) order.Cut();
-            else if (menuChoice == 3)
-            {
-                order.Eat();
-
-                if (order.FoodIsEaten) orders.RemoveAt(index);
-
-                WaitForUser();
-            }
-                
-                
-            else if (menuChoice == 4) return;
-        }
     }
 }
--- Restaurant/InterfaceClasses/OrdersMenu.cs
+++ Restaurant/InterfaceClasses/OrdersMenu.cs
@@ -10,5 +10,116 @@ namespace Restaurant.InterfaceClasses
     internal class OrdersMenu
     {
         private List<Food> orders = new List<Food>();
+
+        // добавляет блюдо в список заказов из списка меню по индексу
+        internal void MakeOrderByItem(Food foodItem)
+        {
+            switch (foodItem)
+            {
+                case Pizza pizza:
+                    var (pizzaName, size) = pizza;
+                    orders.Add(new Pizza(pizzaName, size));
+                    break;
+                case Calzone calzone:
+                    var (calzoneName, calzoneWeight) = calzone;
+                    orders.Add(new Calzone(calzoneName, calzoneWeight));
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        // вывод меню взаимодействия с заказами
+        public void ShowOrdersMenu()
+        {
+            int i = 0, menuChoice;
+
+            Console.Clear();
+            Console.WriteLine("Меню взаимодействия с заказами:");
+            for (; i < orders.Count; i++)
+            {
+                Console.Write(i + 1 + ".");
+                orders[i].PrintFoodShort(); // выводим элементы списка заказов
+            }
+            Console.WriteLine(++i + ".Выйти в основное меню.");
+
+            Console.Write("Ваш выбор (цифра 1-{0}): ", i);
+            string? choiceStr = Console.ReadLine(); // получаем строку с выбором
+
+            menuChoice = Kitchen.CheckChoiseMenu(choiceStr, i);
+
+            // if (menuChoice <= menu.Count) MakeOrderByIndex(menuChoice - 1); // если выбор не вышел за пределы списка меню
+            if (menuChoice <= orders.Count) ShowOrderMenuByIndex(menuChoice - 1); // если выбор не вышел за пределы списка меню
+        }
+
+
+
+        // Вывод информации и действия с заказом
+        private void ShowOrderMenuByIndex(int index)
+        {
+            int menuChoice, i = 3;
+
+            Console.Clear();
+            orders[index].PrintFoodShort();
+            Console.WriteLine("Меню заказа:");
+            Console.WriteLine("1.Вывести подробную информацию");
+            Console.WriteLine("2.Взаимодействовать с заказом");
+            Console.WriteLine("3.Выйти в главное меню");
+
+            Console.Write("Ваш выбор (цифра 1-{0}): ", i);
+            string? choiceStr = Console.ReadLine(); // получаем строку с выбором
+
+            menuChoice = Kitchen.CheckChoiseMenu(choiceStr, i);
+
+            if (menuChoice == 1) ShowFullOrderInfo(index);
+            else if (menuChoice == 2) ShowInteractionMenuByIndex(index);
+            else if (menuChoice == 3) return;
+        }
+
+        // вывести информацию о заказе
+        private void ShowFullOrderInfo(int index)
+        {
+            Console.Clear();
+            Console.WriteLine("Полная информация о заказе:");
+            orders[index].PrintFoodFull();
+            Console.WriteLine("Далее...");
+            Console.ReadKey();
+        }
+
+
+
+        // вывести меню взаимодействия с заказами
+        private void ShowInteractionMenuByIndex(int index)
+        {
+            int menuChoice, i = 4;
+            Food order = orders[index];
+
+            Console.Clear();
+            order.PrintFoodShort();
+            Console.WriteLine("Варианты взаимодействия:");
+            Console.WriteLine("1.Запечь");
+            Console.WriteLine("2.Нарезать");
+            Console.WriteLine("3.Съесть");
+            Console.WriteLine("4.Выйти из меню");
+
+            Console.Write("Ваш выбор (цифра 1-{0}): ", i);
+            string? choiceStr = Console.ReadLine(); // получаем строку с выбором
+
+            menuChoice = Kitchen.CheckChoiseMenu(choiceStr, i);
+
+            if (menuChoice == 1) order.Bake();
+            else if (menuChoice == 2) order.Cut();
+            else if (menuChoice == 3)
+            {
+                order.Eat();
+
+                if (order.FoodIsEaten) orders.RemoveAt(index);
+
+                Kitchen.WaitForUser();
+            }
+
+
+            else if (menuChoice == 4) return;
+        }
     }
 }
